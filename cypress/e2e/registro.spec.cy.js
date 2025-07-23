6b10dbabf6fa22de5371046d6924ab867acde1d2
lopezandres07 <andlop17@gmail.com>
2025-07-23 19:17:21 -0400

    test: add test to check missing params

--- cypress/e2e/registro.spec.cy.js
+++ cypress/e2e/registro.spec.cy.js
@@ -5,27 +5,43 @@ describe('Registro de Usuario', () => {
   })
 
   it('Registro Exitoso con Datos Válidos', () => {
-    // Dado que estoy en la página de registro
     cy.contains('Registro de Usuario')
 
-    // Cuando ingreso los datos válidos
     cy.get('#name').type('Juan Perez')
     cy.get('#email').type('juan.perez@example.com')
     cy.get('#password').type('ClaveSegura123')
     cy.get('#confirmPassword').type('ClaveSegura123')
 
-    // Y hago clic en "Registrarse"
     cy.contains('Registrarse').click()
 
-    // Entonces debería ver el mensaje de éxito
     cy.contains('¡Registro exitoso! Datos enviados correctamente.').should(
       'be.visible'
     )
 
-    // Y el formulario debería estar vacío
     cy.get('#name').should('have.value', '')
     cy.get('#email').should('have.value', '')
     cy.get('#password').should('have.value', '')
     cy.get('#confirmPassword').should('have.value', '')
+  })
+})
+
+describe('Escenario 2: Registro Fallido por Falta de Datos Obligatorios', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('Debe mostrar mensajes de error cuando se omiten campos obligatorios', () => {
+    cy.get('input[name="email"]').type('test@example.com')
+    cy.get('input[name="password"]').type('MiClave123')
+    cy.get('input[name="confirmPassword"]').type('MiClave123')
+    cy.get('button[type="submit"]').click()
+
+    cy.contains('El nombre es obligatorio.').should('be.visible')
+
+    cy.contains('Por favor, corrige los errores en el formulario.').should(
+      'be.visible'
+    )
+
+    cy.contains('¡Registro exitoso!').should('not.exist')
   })
 })
